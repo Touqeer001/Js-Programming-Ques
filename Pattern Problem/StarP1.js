2d1cf693923b8bee5fee3904170d14da8306a640
Touqeer Ansari <126690073+Touqeer001@users.noreply.github.com>
2025-01-14 06:03:35 +0530

    Update StarP1.js

--- Pattern Problem/StarP1.js
+++ Pattern Problem/StarP1.js
@@ -8,16 +8,16 @@ console.log("TOuqeer IBN KHursheed")
 // ****
 
 // console.log("")
-// let n=5;
-// for(let i=0;i<=n;i++){
-//     let row=""
-//     for(j=1;j<=n;j++){
-//         row+=""
-//     }
-//     console.log("*")
+let n=5;
+ for(let i=0;i<=n;i++){
+  let row=""
+    for(j=1;j<=n;j++){
+        row+=""
+     }
+     console.log("*")
     
-// }
-// console.log("end")
+ }
+ console.log("end")
 
 
 //!2. Print a Right-Angle Triangle of `*`
@@ -28,15 +28,15 @@ console.log("TOuqeer IBN KHursheed")
 // ***
 // ****
 
-// let n=4;
-// for(let row=1;row<=n;row++)
-//     // let rows="";
-// for(let col=1;col<=row;col++)
-//     // rows+=""
-//     // console.log('')
+ let n=4;
+ for(let row=1;row<=n;row++)
+     let rows="";
+ for(let col=1;col<=row;col++)
+      rows+=""
+     console.log('')
     
 
-// console.log('*'.repeat(row))
+console.log('*'.repeat(row))
 
 
 //! 3. Print an Inverted Right-Angle Triangle of `*`
@@ -47,13 +47,13 @@ console.log("TOuqeer IBN KHursheed")
 // **
 // *
 
-// let n=5;
-// for(let i=1;i<=n;i++){
-//     for(j=n;j<=i;j--){
-//          console.log("*")
-//     }
-//     console.log('*'.repeat(j))
-// }
+ let n=5;
+for(let i=1;i<=n;i++){
+    for(j=n;j<=i;j--){
+          console.log("*")
+     }
+    console.log('*'.repeat(j))
+ }
 
 // let n=5;
 // for(let i=1;i<=n;i++){
